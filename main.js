3fea8ab73ad3e32ec0d78f0d8020c3f4a67cc842
Petra Schneiderova <pe.schneiderova@gmail.com>
2023-02-15 14:05:51 +0100

    posun

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 // tady je místo pro náš program
+/*let Jmeno = prompt("Napis sve jmeno");
+let vek = Number(prompt("Jaky je vas vek?"));
+
+document.body.innerHTML += "<p>" + Jmeno + ", vek: " + vek + "</p>";
+const hodinovaSazba = Number(prompt("Jaka je vasa hodinova sazba?"));
+const pocetHodin = Number(prompt("Kolko hodin denne pracujete?"));
+const pocetDni = Number(prompt("Kolko dni mesacne pracujete?"));
+
+document.body.innerHTML += "<p>" + (hodinovaSazba*pocetHodin*pocetDni) + "</p>";
+const hodinovaSazba = Number(prompt("Jaka je vasa hodinova sazba?"));
+const pocetHodin = Number(prompt("Kolko hodin denne pracujete?"));
+const pocetDni = Number(prompt("Kolko dni mesacne pracujete?"));
+​
+document.body.innerHTML += "<p>" + (hodinovaSazba*pocetHodin*pocetDni) + "</p>";
+
+const hodinovaSazba = Number(prompt("Jaka je vasa hodinova sazba?"));
+const pocetHodin = Number(prompt("Kolko hodin denne pracujete?"));
+const pocetDni = Number(prompt("Kolko dni mesacne pracujete?"));
+​
+document.body.innerHTML += "<p>" + (hodinovaSazba*pocetHodin*pocetDni) + "</p>";
+
+*/
+let Jmeno = prompt("Napiš své jméno a příjmení.");
+let vek = Number(prompt("Kolik je vám let?")); 
+let barva = prompt("Jaká je veše nejoblíbenější barva anglicky");
+document.body.innerHTML += "<p class='odstavec'>" + Jmeno + "; " + vek + "</p>";
+document.querySelector('.odstavec')
+odstavec.style.color = 'barva'
